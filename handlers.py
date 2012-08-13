3b4cc1959051e89627a037e5fa9bfe07a025eee6
Sergey Trofimov <truetug@gmail.com>
2012-08-13 11:51:42 +0400

    Rename handler to handlers

---
+++ handlers.py
@@ -0,0 +1,152 @@
+# encoding: utf-8
+import tornado.web
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+
+import time
+import hashlib
+import logging
+from sse import Sse
+
+
+SSE_HEADERS = (
+    ('Content-Type','text/event-stream; charset=utf-8'),
+    ('Cache-Control','no-cache'),
+    ('Connection','keep-alive'),
+)
+
+
+class SSEHandler(tornado.web.RequestHandler):
+    _closing_timeout = False
+    _connections = []
+    _channels = {}
+
+    def __init__(self, application, request, **kwargs):
+        super(SSEHandler, self).__init__(application, request, **kwargs)
+        self.stream = request.connection.stream
+        self._closed = False
+
+    def initialize(self):
+        for name, value in SSE_HEADERS:
+            self.set_header(name, value)
+
+    def get_class(self):
+        return self.__class__
+
+    def get_id(self):
+        logging.info('Generating unique connection ID')
+        return hashlib.md5('%s-%s-%s' % (
+            self.request.connection.address[0],
+            self.request.connection.address[1],
+            time.time(),
+        )).hexdigest()
+
+    def get_channel(self):
+        return self.get_argument('channel', CHANNEL)
+
+    @tornado.web.asynchronous
+    def get(self):
+        # Sending the standard headers: open event
+        headers = self._generate_headers()
+        self.write(headers)
+        self.flush()
+
+        self.connection_id = self.get_id()
+        self.on_open()
+
+    def on_open(self, *args, **kwargs):
+        """ Invoked for a new connection opened. """
+        cls = self.__class__
+        logger.debug('Incoming connection %s to channel "%s"' % (self.connection_id, self.channel))
+
+        self.channel = self.get_channel()
+        if not self.channel:
+            self.set_status(403)
+            self.finish()
+        else:
+            if not self.channel in cls._channels:
+                if cls._channels.keys():
+                    cls.client.unsubscribe(cls._channels.keys())
+
+                cls._channels[self.channel] = set([self])
+                cls.client.subscribe(cls._channels.keys())
+                cls.client.listen(send_message)
+                logger.debug('Channels: %s' % ', '.join(cls._channels.keys()))
+            else:
+                cls._channels[self.channel].add(self)
+
+    def on_close(self):
+        """ Invoked when the connection for this instance is closed. """
+        cls = self.__class__
+
+        logger.debug('Connection %s is closed' % self.connection_id)
+        if len(cls._channels[self.channel]) > 1:
+            cls._channels[self.channel].remove(self)
+        else:
+            del cls._channels[self.channel]
+            logger.debug('Channels: %s' % ', '.join(cls._channels.keys()))
+
+    def close(self):
+        """ Closes the connection for this instance """
+        if not self._closed and not getattr(self, '_closing_timeout', None):
+            self._closed = True
+            self._closing_timeout = tornado.ioloop.IOLoop.instance().add_timeout(time.time() + 5, self._abort)
+        else:
+            tornado.ioloop.IOLoop.instance().remove_timeout(self._closing_timeout)
+            self.on_close() # Calling the closing event
+            self.stream.close()
+
+    def _abort(self):
+        """ Instantly aborts the connection by closing the socket """
+        self._closed = True
+        self.stream.close()
+
+    @classmethod
+    def on_message(cls, msg):
+        """ Sends a message to all live connections """
+        event, data = json.loads(msg.body)
+
+        sse = Sse()
+        sse.add_message(event, data)
+        message = ''.join(sse)
+
+        clients = cls._channels.get(msg.channel, [])
+        logger.debug('Sending %s "%s" to %s clients' % (event, data, len(clients)))
+        for client in clients:
+            client.write_message(message)
+
+    def write_message(self, message):
+        self.write(message)
+        self.flush()
+
+
+class DjangoSSEHandler(SSEHandler):
+    @tornado.web.asynchronous
+    def get_channel(self):
+        self.user = self.get_current_user()
+        return user.username if user else None
+
+    def get_django_session(self):
+        """ Gets django session """
+        from django.utils.importlib import import_module
+        from django.conf import settings
+
+        if not hasattr(self, '_session'):
+            engine = import_module(settings.SESSION_ENGINE)
+            session_key = self.get_cookie(settings.SESSION_COOKIE_NAME)
+            self._session = engine.SessionStore(session_key)
+
+        return self._session
+
+    def get_current_user(self):
+        """ Gets user from request using django session engine """
+        from django.contrib.auth import get_user
+
+        # get_user needs a django request object, but only looks at the session
+        class Dummy: pass
+
+        django_request = Dummy()
+        django_request.session = self.get_django_session()
+        user = get_user(django_request)
+        return user if user.is_authenticated() else None
